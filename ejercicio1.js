5b892875c8056e476de24490d20890313727d1da
Brayan Andres Castro Marquez <bkpunk1992@gmail.com>
2026-03-20 12:03:19 -0500

    15.3---TIFC2DEV-SD-08---Lab---Declaraci-n-de-Clases

---
+++ ejercicio1.js
@@ -0,0 +1,104 @@
+class Empleado {
+
+    constructor(nombre, cargo, salario) {
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.activo = true;
+    }
+
+    presentarse() {
+        return `Hola, soy ${this.nombre} y trabajo como ${this.cargo}.`;
+    }
+
+    calcularSalarioAnual() {
+        return this.salario * 12;
+    }
+
+    desactivar() {
+        this.activo = false;
+        return `${this.nombre} ha sido desactivado.`;
+    }
+
+    info() {
+        return `Nombre: ${this.nombre} | Cargo: ${this.cargo} | Salario: $${this.salario} | Activo: ${this.activo}`;
+    }
+}
+
+const emp1 = new Empleado("Ana", "Desarrolladora", 3000);
+const emp2 = new Empleado("Luis", "QA Tester", 2500);
+const emp3 = new Empleado("Carlos", "Diseñador UX", 2800);
+
+console.log(emp1.presentarse());
+console.log(emp1.calcularSalarioAnual());
+console.log(emp1.info());
+
+console.log(emp2.presentarse());
+console.log(emp2.calcularSalarioAnual());
+console.log(emp2.info());
+
+console.log(emp3.presentarse());
+console.log(emp3.calcularSalarioAnual());
+console.log(emp3.info());
+
+console.log(emp2.desactivar());
+console.log(emp2.info());
+
+class Lider extends Empleado {
+
+    constructor(nombre, salario, equipo) {
+        super(nombre, "Lider de equipo", salario);
+        this.equipo = equipo;
+    }
+
+    presentarEquipo() {
+        return `Mi equipo está formado por: ${this.equipo.join(", ")}`;
+    }
+
+    agregarMiembro(nombre) {
+        this.equipo.push(nombre);
+        return `${nombre} fue agregado al equipo.`;
+    }
+
+    info() {
+        return `${super.info()} | Personas en equipo: ${this.equipo.length}`;
+    }
+}
+
+const lider1 = new Lider("María", 4500, ["Ana", "Luis"]);
+const lider2 = new Lider("Pedro", 4700, ["Carlos"]);
+
+console.log(lider1.calcularSalarioAnual());
+console.log(lider1.desactivar());
+
+console.log(lider1.agregarMiembro("Sofia"));
+console.log(lider1.presentarEquipo());
+
+const e1 = new Empleado("Juan", "Backend Dev", 3200);
+const e2 = new Empleado("Laura", "Frontend Dev", 3100);
+const e3 = new Empleado("Mateo", "Soporte", 2000);
+const e4 = new Empleado("Elena", "Analista", 2900);
+
+const l1 = new Lider("Andrés", 5000, ["Juan", "Laura"]);
+const l2 = new Lider("Camila", 5200, ["Mateo", "Elena"]);
+
+const empleados = [e1, e2, e3, e4, l1, l2];
+
+let costoMensual = 0;
+
+for (let emp of empleados) {
+    costoMensual += emp.salario;
+}
+
+console.log("Costo mensual nómina:", costoMensual);
+
+console.log("Costo anual nómina:", costoMensual * 12);
+
+e3.desactivar();
+console.log(e3.info());
+
+console.log(l1.info());
+console.log(l1.presentarEquipo());
+
+console.log(l2.info());
+console.log(l2.presentarEquipo());
---
+++ ejercicio2.js
@@ -0,0 +1,115 @@
+class Contenido {
+
+    constructor(titulo, genero, anio) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.anio = anio;
+        this.disponible = true;
+    }
+
+    ficha() {
+        return `${this.titulo} | ${this.genero} | ${this.anio}`;
+    }
+
+    retirar() {
+        this.disponible = false;
+        return `${this.titulo} ha sido retirado del catálogo.`;
+    }
+
+    estado() {
+        return this.disponible
+            ? `${this.titulo} está disponible`
+            : `${this.titulo} está retirado`;
+    }
+}
+
+class Pelicula extends Contenido {
+
+    constructor(titulo, genero, anio, duracion) {
+        super(titulo, genero, anio);
+        this.duracion = duracion;
+    }
+
+    duracionFormateada() {
+        const horas = Math.floor(this.duracion / 60);
+        const minutos = this.duracion % 60;
+        return `${horas}h ${minutos}min`;
+    }
+
+    ficha() {
+        return `${super.ficha()} | Duración: ${this.duracionFormateada()}`;
+    }
+}
+
+class Serie extends Contenido {
+
+    constructor(titulo, genero, anio, temporadas) {
+        super(titulo, genero, anio);
+        this.temporadas = temporadas;
+        this.episodiosPorTemporada = 0;
+    }
+
+    registrarEpisodios(cantidad) {
+        this.episodiosPorTemporada = cantidad;
+    }
+
+    totalEpisodios() {
+        return this.temporadas * this.episodiosPorTemporada;
+    }
+
+    ficha() {
+        return `${super.ficha()} | Temporadas: ${this.temporadas} | Episodios totales: ${this.totalEpisodios()}`;
+    }
+}
+
+const peli1 = new Pelicula("Inception", "Sci-Fi", 2010, 148);
+const peli2 = new Pelicula("Coco", "Animación", 2017, 105);
+
+const serie1 = new Serie("Stranger Things", "Sci-Fi", 2016, 4);
+serie1.registrarEpisodios(9);
+
+const serie2 = new Serie("Dark", "Misterio", 2017, 3);
+serie2.registrarEpisodios(10);
+
+console.log(peli1.ficha());
+console.log(peli2.ficha());
+console.log(serie1.ficha());
+console.log(serie2.ficha());
+
+console.log(peli2.retirar());
+console.log(peli2.estado());
+
+const peli3 = new Pelicula("Avatar", "Acción", 2009, 162);
+
+const serie3 = new Serie("Breaking Bad", "Drama", 2008, 5);
+serie3.registrarEpisodios(13);
+
+const catalogo = [
+    peli1,
+    peli2,
+    peli3,
+    serie1,
+    serie2,
+    serie3
+];
+
+console.log("\n CATÁLOGO ");
+for (let contenido of catalogo) {
+    console.log(contenido.ficha());
+}
+
+peli1.retirar();
+serie2.retirar();
+
+console.log(peli1.estado());
+console.log(serie2.estado());
+
+let disponibles = 0;
+
+for (let contenido of catalogo) {
+    if (contenido.disponible === true) {
+        disponibles++;
+    }
+}
+
+console.log("Contenidos disponibles:", disponibles);
